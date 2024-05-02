fe9b522208d5b2765e8e60c6e49a801f01063d99
Augusto Garcia da Rosa <augusto.garciadarosa@gmail.com>
2024-05-02 16:48:29 -0300

    feat: implementação do A*

--- src/busca/a_star.py
+++ src/busca/a_star.py
@@ -1,5 +1,41 @@
 """Implementação do algoritmo A*."""
-
+from queue import PriorityQueue
+from util import haversine
 
 def a_star(graph, start: int, goal: int) -> (int, float, [int]):
     """Busca em graph, um caminho entre start e goal usando A*."""
+    frontier = PriorityQueue()
+    frontier.put(start, 0)
+    came_from = {}
+    const_so_far = {}
+    came_from[start] = None
+    const_so_far[start] = 0
+
+    while not frontier.empty():
+        current = frontier.get()
+
+        if current == goal:
+            predecessor = came_from[current]
+            path = [current]
+            while predecessor != None:
+                path.append(predecessor)
+                predecessor = came_from[predecessor]
+            path.reverse()
+            break
+
+
+        for next_node, next_node_cost in graph[current][1].items():
+            new_cost = const_so_far[current] + next_node_cost
+
+            if next_node not in const_so_far or new_cost < const_so_far[next_node]:
+                const_so_far[next_node] = new_cost
+                priority = new_cost + haversine(
+                    graph[goal][0][0],
+                    graph[goal][0][1],
+                    graph[next_node][0][0], 
+                    graph[next_node][0][1] 
+                )
+                frontier.put(next_node, priority)
+                came_from[next_node] = current
+    
+    return (len(path) - 1, const_so_far[goal], path)
--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 """Utilize este arquivo para depurar seus algoritmos."""
 
 from graph import read_graph
-from busca import dfs
+from busca import a_star
 
 
 if __name__ == "__main__":
     grafo = read_graph("C:\www\lasalle\ia-2024-t1\mapas\mini_map.txt")
 
-    caminho = dfs(grafo, 0, 6)
-    print(caminho)
+    vertices_avaliados, custo, caminho = a_star(grafo, 0, 7)
+    print(vertices_avaliados, custo, caminho)
