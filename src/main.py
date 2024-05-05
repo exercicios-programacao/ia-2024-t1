33ed9c61220213f8403f510df532684371bfad82
joao <joao.202211948@unilasalle.edu.br>
2024-05-04 22:57:29 -0300

    feat: main

--- src/main.py
+++ src/main.py
@@ -1,10 +1,8 @@
-"""Utilize este arquivo para depurar seus algoritmos."""
-
 from graph import read_graph
 from busca import a_star
 
 
 if __name__ == "__main__":
-    grafo = read_graph("vertices.txt")
+    grafo = read_graph("mini_map.txt")
     vertices_avaliados, custo, caminho = a_star(grafo, 1, 100)
     print(vertices_avaliados, custo, caminho)
