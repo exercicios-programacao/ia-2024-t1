dd5def3516574fd558f62ffaa1b4b483a15bb778
Danielle Parmigiani Werutsky <daniwerutsky@gmail.com>
2024-05-05 07:59:42 -0300

    fix flake

--- src/busca/bfs.py
+++ src/busca/bfs.py
@@ -13,12 +13,12 @@ def bfs(graph, start, goal):
     while queue:
         v, path = queue.popleft()
         if v == goal:
-            path_length = sum(haversine(graph[path[i]]['coordinates'][0],
+            length = sum(haversine(graph[path[i]]['coordinates'][0],
                                         graph[path[i]]['coordinates'][1],
                                         graph[path[i + 1]]['coordinates'][0],
                                         graph[path[i + 1]]['coordinates']
                                         [1]) for i in range(len(path) - 1))
-            return (len(visited), path_length, path)
+            return (len(visited), length, path)
         if v not in visited:
             visited.add(v)
             for u in graph[v]['edges']:
--- src/util.py
+++ src/util.py
@@ -1,4 +1,4 @@
-"""Funções auxiliares para o projeto."""
+"""Funções auxiliares para o projeto"""
 
 import math
 
@@ -12,10 +12,8 @@ def haversine(lat1, lon1, lat2, lon2):
     lat2 = (lat2) * math.pi / 180.0
     # apply formulae
     a = (
-        pow(math.sin(dLat / 2), 2) +
-        pow(math.sin(dLon / 2), 2) *
-        math.cos(lat1) * math.cos(lat2)
-        )
+        pow(math.sin(dLat / 2), 2) + pow(math.sin(dLon / 2), 2) * math.cos(lat1) * math.cos(lat2)
+    )
     rad = 6371
     c = 2 * math.asin(math.sqrt(a))
     return (rad * c) * 1000
